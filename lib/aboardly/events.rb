d2fc1933b4d482d3cec3cb564685acbb214f68b1
Martin Wong <martimus.wong@gmail.com>
2016-05-14 18:25:42 -0400

    api can now send event properties

--- lib/aboardly/events.rb
+++ lib/aboardly/events.rb
@@ -6,8 +6,11 @@ class Customers
     @base_url = base_url
   end
 
-  def create(customer_id, event_name)
-    options = {basic_auth: @auth}
+  def create(customer_id, event_name, *properties)
+    if (properties.empty?)
+      properties = [{}]
+    end
+    options = {basic_auth: @auth, body: properties[0].to_json, :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}}
     self.class.post("#{@base_url}/customers/#{customer_id}/events/#{event_name}", options)
   end
 end
